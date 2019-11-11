8bf909708cda5b2ac11d6e95b9928a4f5d21831e
Niklas Kühtmann <nk@ized.io>
2019-11-11 03:49:17 +0100

    Update demo

--- src/components/ctx-greet-comp/ctx-greet-comp.tsx
+++ src/components/ctx-greet-comp/ctx-greet-comp.tsx
@@ -1,9 +1,18 @@
-import { Component, h } from '@stencil/core';
+import { Component, h, Element } from '@stencil/core';
+import { WatchContext } from '../../libs/context';
 
 @Component({
     tag: 'ctx-greet-comp'
 })
-export class CtxGreetComp {
+export class CtxGreetComp 
+{
+    @Element() el!: HTMLCtxGreetCompElement;
+
+    @WatchContext()
+    greeting(v: string) {
+        console.log("New greeting", v);
+    }
+
     render() {
         return <div>
             Hallo <ctx-consumer name="greeting"></ctx-consumer>
