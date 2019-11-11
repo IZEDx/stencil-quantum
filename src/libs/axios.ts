f1daf9f8dbd7ce4613ee1e27916d374a730acad2
Niklas Kühtmann <nk@ized.io>
2019-11-11 03:48:47 +0100

    ♻ Refactor axios

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -1,5 +1,5 @@
 import { Provider } from "./provider";
-import { getEl, hookComponent, ComponentPrototype } from "./utils";
+import { getEl, hookComponent, ComponentPrototype, log } from "./utils";
 import { HTMLStencilElement } from "@stencil/core/internal";
 import { RestypedBase } from "restyped";
 
@@ -10,49 +10,222 @@ export interface AxiosResponse<T>
 
 export interface AxiosInstance
 {
-    get(path: string, config?: { params?: Record<string, string> }): Promise<AxiosResponse<any>>;
+    get(path: string, config?: { 
+        params?: Record<string, string> 
+    }): Promise<AxiosResponse<any>>;
+    put(path: string, data: any, config?: { 
+        params?: Record<string, string>
+    }): Promise<AxiosResponse<any>>;
 }
 
-type ValidPrototype<Schema extends RestypedBase, Path extends ValidPath<Schema>, Method extends "GET"|"POST"|"PUT", K extends string> = 
-    Schema[Path][Method] extends undefined ? never : ComponentPrototype&{
-        [key in K]: Schema[Path][Method]["response"]
-    };
+type Methods = "GET"|"POST"|"PUT"|"DELETE";
+type ValidPrototype<Schema extends RestypedBase, Path extends ValidPath<Schema>, Method extends Methods> = 
+    Schema[Path][Method] extends undefined ? never : ComponentPrototype;
 type ValidPath<Schema extends RestypedBase> = Schema extends RestypedBase ? keyof Schema : string;
 
+export function Rest<
+    Schema extends RestypedBase = any, 
+    Path extends ValidPath<Schema> = ValidPath<Schema>, 
+    Proto extends ValidPrototype<Schema, Path, "GET"> = ValidPrototype<Schema, Path, "GET">
+>(axiosKey: string, path: Path, paramsKey?: string, responseKey?: string)
+{ 
+    return Http<Schema, Path, "GET", Proto>(axiosKey, path, paramsKey, 
+    {
+        init(ctx) {
+            ctx.sendNext = false;
+        },
+
+        onComponent(ctx) {
+            if (responseKey !== undefined) {
+                ctx.responseProvider = Provider.create(ctx.el!, responseKey!, undefined);
+            }
+        },
+
+        onAxios(ctx) {
+            if (!paramsKey || !!ctx.params) ctx.fns!.send!(ctx as any);
+        },
+
+        onParams(ctx) {
+            if (!!ctx.axios) ctx.fns!.send!(ctx as any);
+        },
+
+        async send(ctx) {
+            if (ctx.sendNext) {
+                ctx.sendNext = false;
+                log("Sending Rest PUT", ctx);
+                const response = await ctx.axios.put(ctx.query.path, ctx.value, {params: ctx.query.params});
+                if (ctx.responseProvider) {
+                    ctx.responseProvider.provide(response);
+                }
+            }
+            log("Sending Rest GET", ctx);
+            const response = await ctx.axios.get(ctx.query.path, {params: ctx.query.params});
+            ctx.value = response.data;
+            ctx.el.forceUpdate();
+        },
+    
+        setValue(value, ctx) {
+            ctx.value = value;
+
+            ctx.sendNext = true;
+            if (!!ctx.axios && (!paramsKey || !!ctx.params)) {
+                ctx.fns!.send!(ctx as any);
+            }
+
+            if (ctx.el) ctx.el.forceUpdate();
+        },
+
+        getValue(ctx) {
+            return ctx.value;
+        }
+        
+    });
+} 
 
 export function Get<
     Schema extends RestypedBase = any, 
     Path extends ValidPath<Schema> = ValidPath<Schema>, 
-    PKey extends string = string,
-    Proto extends ValidPrototype<Schema, Path, "GET", PKey> = ValidPrototype<Schema, Path, "GET", PKey>
+    Proto extends ValidPrototype<Schema, Path, "GET"> = ValidPrototype<Schema, Path, "GET">
 >(axiosKey: string, path: Path, paramsKey?: string)
 { 
-    return function (prototype: Proto, propertyName: PKey)
+    return Http<Schema, Path, "GET", Proto>(axiosKey, path, paramsKey, 
     {
-        let value: any = prototype[propertyName];
-        let el: HTMLStencilElement;
-        let axios: AxiosInstance;
-        let params: Record<string, any>;
+        onAxios(ctx) {
+            if (!paramsKey || !!ctx.params) ctx.fns!.send!(ctx as any);
+        },
 
-        const get = async (params?: Record<string, any>) => {
-            const query = makeQuery(path as string, params);
-            const response = await axios.get(query.path, {params: query.params});
-            value = response.data;
-            el.forceUpdate();
+        onParams(ctx) {
+            if (!!ctx.axios) ctx.fns!.send!(ctx as any);
+        },
+
+        async send(ctx) {
+            log("Sending GET", ctx);
+            const response = await ctx.axios.get(ctx.query.path, {params: ctx.query.params});
+            ctx.value = response.data;
+            ctx.el.forceUpdate();
+        },
+    
+        setValue(value, ctx) {
+            ctx.value = value;
+            if (ctx.el) ctx.el.forceUpdate();
+        },
+
+        getValue(ctx) {
+            return ctx.value;
         }
+        
+    });
+} 
+
+export function Put<
+    Schema extends RestypedBase = any, 
+    Path extends ValidPath<Schema> = ValidPath<Schema>, 
+    Proto extends ValidPrototype<Schema, Path, "PUT"> = ValidPrototype<Schema, Path, "PUT">
+>(axiosKey: string, path: Path, paramsKey?: string, responseKey?: string)
+{ 
+    return Http<Schema, Path, "PUT", Proto>(axiosKey, path, paramsKey, 
+    {
+        init(ctx) {
+            ctx.onReady = () => {};
+        },
+
+        onComponent(ctx) {
+            if (responseKey !== undefined) {
+                ctx.responseProvider = Provider.create(ctx.el!, responseKey!, undefined);
+            }
+        },
+
+        onAxios(ctx) {
+            if (!paramsKey || !!ctx.params) ctx.onReady();
+        },
+
+        onParams(ctx) {
+            if (!!ctx.axios) ctx.onReady();
+        },
+
+        async send(ctx) {
+            log("Sending PUT", ctx);
+            const response = await ctx.axios.put(ctx.query.path, ctx.value, {params: ctx.query.params});
+            if (ctx.responseProvider) {
+                ctx.responseProvider.provide(response);
+            }
+        },
+    
+        setValue(value, ctx) {
+            ctx.value = value;
+            if (!!ctx.axios && (!paramsKey || !!ctx.params)) {
+                ctx.fns!.send!(ctx as any);
+            } else {
+                ctx.onReady = () => ctx.fns!.send!(ctx as any);
+            }
+            
+            if (ctx.el) ctx.el.forceUpdate();
+        },
+
+        getValue(ctx) {
+            return ctx.value;
+        }
+        
+    });
+} 
+
+interface HttpContext<Schema> {
+    proto: ComponentPrototype;
+    propertyName: string;
+    obj: any;
+    value: any;
+    el: HTMLStencilElement;
+    axios: AxiosInstance;
+    path: ValidPath<Schema>;
+    params?: Record<string, any>;
+    fns: HttpCallbacks<Schema>;
+    [key: string]: any;
+}
+
+interface HttpCallbacks<Schema> {
+    setValue(value: any, ctx: Partial<HttpContext<Schema>>): void;
+    getValue(ctx: Partial<HttpContext<Schema>>): any;
+    init?(ctx: Partial<HttpContext<Schema>>): void;
+    send?(ctx: HttpContext<Schema>): void|Promise<void>;
+    onComponent?(ctx: Partial<HttpContext<Schema>>): void|Promise<void>;
+    onAxios?(ctx: Partial<HttpContext<Schema>>): void|Promise<void>;
+    onParams?(ctx: Partial<HttpContext<Schema>>): void|Promise<void>;
+}
+
+function Http<
+    Schema extends RestypedBase = any, 
+    Path extends ValidPath<Schema> = ValidPath<Schema>, 
+    Method extends Methods = "GET",
+    Proto extends ValidPrototype<Schema, Path, Method> = ValidPrototype<Schema, Path, Method>
+>(axiosKey: string, path: Path, paramsKey: string|undefined, fns: HttpCallbacks<Schema>)
+{ 
+    return function (prototype: Proto, propertyName: string)
+    {
+        const ctx: Partial<HttpContext<Schema>> = {
+            proto: prototype,
+            propertyName,
+            value: prototype[propertyName],
+            path,
+            fns,
+            query: makeQuery(path as string)
+        };
+        if (ctx.fns!.init) ctx.fns!.init(ctx);
 
         hookComponent(prototype, async obj => {
-            el = getEl(obj);            
+            ctx.obj = obj;
+            ctx.el = getEl(ctx.obj);          
+            if (ctx.fns!.onComponent) await ctx.fns!.onComponent(ctx);
 
-            Provider.find<AxiosInstance>(el, axiosKey).listen(async a => {
-                axios = a
-                if (!paramsKey || !!params) get();
+            Provider.find<AxiosInstance>(ctx.el, axiosKey).listen(async a => {
+                ctx.axios = a;
+                if (ctx.fns && ctx.fns.onAxios) await ctx.fns.onAxios(ctx);
             });        
             
             if (paramsKey !== undefined)
-            Provider.find<Record<string, any>>(el, paramsKey).listen(async p => {
-                params = p;
-                if (!!axios) get();
+            Provider.find<Record<string, any>>(ctx.el, paramsKey).listen(async p => {
+                ctx.params = p;
+                ctx.query = makeQuery(ctx.path as string, ctx.params);
+                if (ctx.fns && ctx.fns.onParams) await ctx.fns.onParams(ctx);
             });
 
         });
@@ -61,11 +234,8 @@ export function Get<
         {
             Object.defineProperty(prototype, propertyName, 
             {
-                get: () => value,
-                set: (v) => {
-                    value = v;
-                    if (el) el.forceUpdate();
-                },
+                get: () => ctx.fns && ctx.fns.getValue(ctx),
+                set: (v) => ctx.fns && ctx.fns.setValue(v, ctx),
                 enumerable: true,
                 configurable: true
             });
